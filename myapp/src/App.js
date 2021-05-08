ee4f8b8011f58c15d0fb211d97d6056f7deac966
pant411 <pantchanit2561@hotmail.com>
2021-05-08 16:13:49 +0700

    add table

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  /*return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,6 +18,28 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
+  );*/
+  return(
+    <div className="App">
+      <h1>Hello ReactWorld!!!</h1>
+      <table border="1">
+        <tr>
+          <td>รหัสสินค้า</td>
+          <td>ชื่อหนังสือ</td>
+          <td>ราคาสินค้า</td>
+        </tr>
+        <tr>
+          <td>0788</td>
+          <td>การใช้งานภาษา c#</td>
+          <td>355</td>
+        </tr>   
+        <tr>
+          <td>1788</td>
+          <td>การใช้งานภาษา javascript</td>
+          <td>112</td>
+        </tr>        
+      </table>
     </div>
   );
 }
