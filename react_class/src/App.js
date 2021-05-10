ac4d45ccb83dfabefda6710509eb0b5eca736bf1
pant411 <pantchanit2561@hotmail.com>
2021-05-11 00:57:14 +0700

    add constructor

--- react_class/src/App.js
+++ react_class/src/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
-import Home from './Home';
 
 class App extends Component{
-  render(){
-    const txtStyle = {
-      color: 'white',
-      backgroundColor: 'gray'
-    };
-  
-    return (
-      <div>
-        <h1 style={txtStyle}>Hello Prayut</h1>
-        <Home name = "My Mother Home" address = "Bangkok" />
-      </div>
-    );
-  }
+    constructor(){
+       super();
+       this.state = {
+          data: 0
+       } ;
+    }
+
+    addData = () => {
+      this.setState(d => (
+        {
+          data: d.data + 1
+        }
+      ))
+    }
+
+    render() {
+      return (
+        <div>
+          <p>ข้อมูลปัจจุบัน : {this.state.data}</p>
+          <button onClick={this.addData}>คลิก</button>
+        </div>
+      );
+    }
 }
 
 export default App;
